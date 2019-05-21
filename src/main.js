3fca4ce0617eed9e6928b3176541faee643d8ddc
Sirus Goorhuis <sirus.goorhuis@MacBook-Pro-van-Sirus.local>
2019-05-21 09:39:53 +0200

    Addes Vuesax to experiment with this CSS library

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue';
+import Vuesax from 'vuesax'
 import App from './App.vue';
+import 'vuesax/dist/vuesax.css' //Vuesax styles 
 
+Vue.use(Vuesax)
 Vue.config.productionTip = false;
 
 new Vue({
   render: h => h(App),
 }).$mount('#app');
+
+
+
+
+
+
+
